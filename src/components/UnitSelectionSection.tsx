72315514bda7b006bd40b4d63b253891e34afedd
John Wattenbarger <johncwattenbarger@gmail.com>
2023-11-28 05:48:24 -0600

    Updated styling

--- src/components/UnitSelectionSection.tsx
+++ src/components/UnitSelectionSection.tsx
@@ -42,7 +42,7 @@ const UnitSelectionSection: React.FC<UnitSelectionSectionProps> = ({ title, unit
     return (
         <Stack
             verticalAlign="start"
-            tokens={{ childrenGap: 20 }}
+            tokens={{ childrenGap: 50 }}
             className={`unit-selection-section ${title.toLowerCase()}`}
             style={{ position: 'relative' }}
         >
@@ -50,16 +50,15 @@ const UnitSelectionSection: React.FC<UnitSelectionSectionProps> = ({ title, unit
                 <Label>{title}</Label>
             </div>
             <UnitSelectionSummary units={units} style={{ position: 'absolute', top: 0, right: 0 }} />
-            <Stack horizontal tokens={{ childrenGap: 10 }}>
+            <Stack horizontal tokens={{ childrenGap: 20 }}>
                 {unitStats.map((unit) => (
-                    <Stack key={unit.type} horizontalAlign="center">
+                    <Stack horizontal key={unit.type} verticalAlign='center' horizontalAlign="center" tokens={{ childrenGap: 6 }}>
                         <Image src={imageMappings[unit.type]} alt={unit.type} width={50} height={50} />
                         <Dropdown
-                            placeholder="Select quantity..."
-                            label="Quantity"
                             options={[...Array(16)].map((_, index) => ({ key: index + 1, text: (index + 1).toString() }))}
                             onChange={(_, option) => handleQuantityChange(unit, option?.key as number)}
                             selectedKey={units.find(u => u.unit === unit)?.quantity}
+                            style={{ minWidth: 65 }}
                         />
                     </Stack>
                 ))}
